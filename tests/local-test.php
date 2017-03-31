81846edfda6d536b3e3956d10f53fcc7e45b9017
sergey144010 <sergey144010@mail.ru>
2017-04-01 06:23:47 +0800

    add

--- tests/local-test.php
+++ tests/local-test.php
@@ -1,7 +1,10 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Мария
- * Date: 01.04.2017
- * Time: 6:06
- */
+namespace tests;
+
+require_once('../HtmlCreator/HtmlCreator.php');
+require_once('../HtmlCreator/Html.php');
+
+use sergey144010\HtmlCreator\HtmlCreator;
+use sergey144010\HtmlCreator\Html;
+
+var_dump(Html::getString(['p']));
