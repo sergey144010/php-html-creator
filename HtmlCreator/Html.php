6150e750db4bf0d72b9f6532f6dac37961da9581
sergey144010 <sergey144010@mail.ru>
2017-04-01 22:49:56 +0800

    add new method

--- HtmlCreator/Html.php
+++ HtmlCreator/Html.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: Ìàðèÿ
+ * User: ÐœÐ°Ñ€Ð¸Ñ
  * Date: 01.04.2017
  * Time: 6:17
  */
@@ -23,5 +23,54 @@ class Html
     public static function getString(array $array)
     {
         return HtmlCreator::instance()->create($array)->getHtml();
+    }
+
+    public static function emptyPage()
+    {
+        echo self::getEmptyPage();
+    }
+
+    public static function getEmptyPage()
+    {
+        $body = [
+            ['html',
+                ['head',
+                    ['meta', 'charset'=>'utf-8'],
+                    ['title'],
+                ],
+                ['body']
+            ]
+        ];
+        return HtmlCreator::instance()->create($body)->getHtml();
+    }
+
+    public static function createPage(array $options)
+    {
+        echo self::getPage($options);
+    }
+
+    public static function getPage(array $options)
+    {
+        if(!isset($options['title'])){$options['title'] = null;};
+        if(!isset($options['bodyText'])){$options['bodyText'] = null;};
+        if(!isset($options['body'])){$options['body'] = null;};
+
+        $optionsBody = ['body'];
+        foreach ($options['body'] as $tag) {
+            $optionsBody[$tag];
+        }
+
+
+        $body = [
+            ['html',
+                ['head',
+                    ['meta', 'charset'=>'utf-8'],
+                    ['title'=>$options['title']],
+                ],
+                $optionsBody
+            ]
+        ];
+
+        return HtmlCreator::instance()->create($body)->getHtml();
     }
 }
